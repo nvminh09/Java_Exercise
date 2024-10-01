83c00e3e72ad2098599a535db37c0bcc6487e1d9
nvminh09 <nvminh09@gmail.com>
2024-10-01 22:44:13 +0700

    Chat room

---
+++ src/NetworkProgramming/BaiTap14/ChatRoom_client/ChatRoom_client.java
@@ -0,0 +1,8 @@
+package NetworkProgramming.BaiTap14.ChatRoom_client;
+
+public class ChatRoom_client {
+    public static void main(String[] args) {
+        ChatClient chatClient = new ChatClient();
+        chatClient.StartClient();
+    }
+}
