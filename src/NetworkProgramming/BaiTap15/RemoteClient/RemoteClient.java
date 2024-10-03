0aaac2200955fc9bdf59f7248af764cca3787bd2
nvminh09 <nvminh09@gmail.com>
2024-10-03 23:12:43 +0700

    Control desktop

--- src/NetworkProgramming/BaiTap15/RemoteClient/RemoteClient.java
+++ src/NetworkProgramming/BaiTap15/RemoteClient/RemoteClient.java
@@ -1,2 +1,44 @@
-package NetworkProgramming.BaiTap15.RemoteClient;public class RemoteClient {
+package NetworkProgramming.BaiTap15.RemoteClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class RemoteClient {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 3000);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            Scanner sc = new Scanner(System.in);
+            boolean exit = false;
+            while (!exit) {
+                System.out.println("\nMenu:");
+                System.out.println("1. Shutdown");
+                System.out.println("2. Restart");
+                int choice = sc.nextInt();
+                switch (choice) {
+                    case 1:
+                        writer.println("shutdown");
+                        writer.flush();
+                        System.out.println(reader.readLine());
+                        break;
+                    case 2:
+                        writer.println("restart");
+                        writer.flush();
+                        System.out.println(reader.readLine());
+                        break;
+                    default:
+                        exit = true;
+                        break;
+                }
+            }
+
+            //System.out.println("Connected to " + socket.getInetAddress().getHostAddress());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/NetworkProgramming/BaiTap15/RemoteServer/RemoteServer.java
+++ src/NetworkProgramming/BaiTap15/RemoteServer/RemoteServer.java
@@ -1,2 +1,48 @@
-package NetworkProgramming.BaiTap15.RemoteServer;public class RemoteServer {
+package NetworkProgramming.BaiTap15.RemoteServer;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class RemoteServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(3000);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Connected to " + socket.getInetAddress().getHostAddress());
+                // Create a new thread to handle the client
+                Thread thread = new Thread(
+                        () -> handleClientRequest(socket)
+                );
+                thread.start();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void handleClientRequest(Socket socket) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            while (true) {
+                String request = reader.readLine();
+                System.out.println("Request from client: " + request);
+                if (request.equals("shutdown")) {
+                    Runtime.getRuntime().exec("shutdown -s -t 3600");
+                    writer.println("Shutdown command has been sent to the client");
+                    writer.flush();
+                } else if (request.equals("restart")) {
+                    Runtime.getRuntime().exec("shutdown -r -t 1");
+                    writer.println("Restart command has been sent to the client");
+                    writer.flush();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
